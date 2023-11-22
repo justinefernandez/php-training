7de0f2e732ad4169a847004a59d2873eeca6ecc5
justinefernandez <jfernandez@stratpoint.com>
2023-11-22 10:14:23 +0800

    Lesson 41

--- app/Http/Controllers/Backend/RoleController.php
+++ app/Http/Controllers/Backend/RoleController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Exports\PermissionExport;
+use App\Imports\PermissionImport;
 
 class RoleController extends Controller
 {
@@ -64,4 +67,35 @@ class RoleController extends Controller
         );
         return redirect()->back()->with($notification);
     }
+
+    public function ImportPermission()
+    {
+        return view('backend.pages.permission.import_permission');
+    }
+
+    public function Export()
+    {
+        return Excel::download(new PermissionExport, 'permission.xlsx');
+    }
+
+    public function Import(Request $request)
+    {
+        Excel::import(new PermissionImport, $request->file('import_file'));
+        $notification = array(
+            'message' => 'Permission Imported Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
+
+
+    // <------------- Roles -----------> //
+
+    public function AllRoles() 
+    {
+        $roles = Role::all();
+        return view('backend.pages.roles.all_roles', compact('roles'));
+    }
+
+
 }
--- resources/views/backend/pages/roles/all_roles.blade.php
+++ resources/views/backend/pages/roles/all_roles.blade.php
@@ -4,11 +4,7 @@
 <div class="page-content">
     <nav class="page-breadcrumb">
         <ol class="breadcrumb">
-            <a href="{{ route('add.permission') }}" class="btn btn-inverse-info">Add Permission</a>
-            &nbsp;&nbsp;&nbsp;
-            <a href="{{ route('import.permission') }}" class="btn btn-inverse-warning">Import</a>
-            &nbsp;&nbsp;&nbsp;
-            <a href="{{ route('export') }}" class="btn btn-inverse-danger">Export</a>
+            <a href="{{ route('add.permission') }}" class="btn btn-inverse-info">Add Roles</a>
         </ol>
     </nav>
 
@@ -16,23 +12,21 @@
         <div class="col-md-12 grid-margin stretch-card">
     <div class="card">
         <div class="card-body">
-        <h6 class="card-title">Permission All</h6>
+        <h6 class="card-title">Role All</h6>
         <div class="table-responsive">
             <table id="dataTableExample" class="table">
             <thead>
                 <tr>
                 <th>SL</th>
-                <th>Permission Name</th>
-                <th>Group Name</th>
+                <th>Role Name</th>
                 <th>Action</th>
                 </tr>
             </thead>
             <tbody>
-                @foreach($permissions as $key => $item)
+                @foreach($roles as $key => $item)
                 <tr>
                 <td>{{ $key+1 }}</td>
                 <td>{{ $item->name }}</td>
-                <td>{{ $item->group_name }}</td>
                 <td>
                     <a href="{{ route('edit.permission', $item->id) }}" class="btn btn-inverse-warning">Edit</a>
                     <a href="{{ route('delete.permission', $item->id) }}" class="btn btn-inverse-danger" id="delete">Delete</a>
